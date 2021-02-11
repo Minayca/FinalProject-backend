fb2c30f0ef14594795d0d52dad9ade8f9a48bb57
Minayca <minaycakanberk@gmail.com>
2021-02-11 14:08:27 +0300

    11.02.2021

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    //Tekrar tekrar new'lememek için static yapıyoruz çünkü bu sabit bir yapı.
+    public static class Messages
+    {
+        //public'ler büyük harfle private'lar küçük harfle başlanarak yazılır.
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        internal static string MaintenanceTime = "Sistem bakımda.";
+        internal static string ProductListed = "Ürünler listelendi.";
+    }
+}
